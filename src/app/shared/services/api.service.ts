2a6ae14f7d4e278bafee74f84b00f9df6887963d
Mark Gniteckis <gniteckis@gmail.com>
2021-02-24 19:31:24 +0000

    Task: Create Dashboard Coin Table (#8)

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
 
-import { retry, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-export class Coin {
-  name!: string;
-  market_caps!: number[][];
-  prices!: number[][];
-  total_volumes!: number[][];
+export interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: Date;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: Date;
+  roi?: any;
+  last_updated: Date;
 }
 
 @Injectable({
@@ -25,23 +47,16 @@ export class ApiService {
     }),
   };
 
-  getAllByName(name: string): Observable<any> {
-    return this.httpClient
-      .get<Coin>(this.endpoint + '/coins/' + name)
-      .pipe(retry(1), catchError(this.processError));
-  }
+  getAllCoins(): Observable<Coin[]> {
+    var request = this.endpoint + '/coins/markets';
 
-  getMarketDataByName(name: string): Observable<Coin> {
-    const currency = '?vs_currency=' + 'gbp';
-    const days = '&days=' + 1;
-    const interval = '&interval=daily';
-
-    var request = this.endpoint + '/coins/' + name + '/market_chart';
-    request += currency + days + interval;
+    const currency = 'gbp';
 
     return this.httpClient
-      .get<Coin>(request)
-      .pipe(retry(1), catchError(this.processError));
+      .get<Coin[]>(request, {
+        params: new HttpParams().set('vs_currency', currency),
+      })
+      .pipe(map((res: Coin[]) => res.map((coin: Coin) => coin)));
   }
 
   processError(err: any) {
